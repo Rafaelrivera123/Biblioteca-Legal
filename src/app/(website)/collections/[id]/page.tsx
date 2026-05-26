8a0b64d734030cafb6ea83cf6564c554aaa41fdd
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-26 01:59:17 -0600

    Update page.tsx

--- src/app/(website)/collections/[id]/page.tsx
+++ src/app/(website)/collections/[id]/page.tsx
@@ -1,47 +1,28 @@
-import { auth } from "@/auth";
-import { getCurrentUserSubscription } from "@/helper/subscription";
 import { prisma } from "@/lib/db";
 import { notFound } from "next/navigation";
 import ArticleContainer from "./_components/article-container";
 import CollectionHeader from "./_components/collection-header";
 
-export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 export async function generateStaticParams() {
   const ids = await prisma.document.findMany({
-    select: {
-      id: true,
-    },
+    select: { id: true },
   });
-
   return ids;
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const document = await prisma.document.findUnique({
-    where: {
-      id: params.id,
-    },
+    where: { id: params.id },
   });
-
-  const cu = await auth();
-
-  const cs = await getCurrentUserSubscription();
-
-  const isActive = cs?.subscription.isActive ?? false;
-  const currentPeriodEnd = cs?.subscription.currentPeriodEnd;
-
-  const now = new Date();
-  const hasFullAccess =
-    isActive && !!currentPeriodEnd && currentPeriodEnd > now && !!cu;
 
   if (!document) notFound();
 
   return (
     <div>
-      <CollectionHeader document={document} hasFullAccess={hasFullAccess} />
-
-      <ArticleContainer documentId={params.id} isLoggedin={!!cu} />
+      <CollectionHeader document={document} hasFullAccess={true} />
+      <ArticleContainer documentId={params.id} isLoggedin={true} />
     </div>
   );
 };
