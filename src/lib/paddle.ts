bc11a015b1f519c7a562b9bc602956311323ac60
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-26 00:52:21 -0600

    Update paddle.ts

--- src/lib/paddle.ts
+++ src/lib/paddle.ts
@@ -1,7 +1,8 @@
 import { Environment, LogLevel, Paddle } from "@paddle/paddle-node-sdk";
-const apiKey = process.env.PADDLE_API_KEY!;
+
+const apiKey = process.env.PADDLE_API_KEY || "placeholder";
 
 export const paddle = new Paddle(apiKey, {
-  environment: Environment.production, // or Environment.sandbox for accessing sandbox API
-  logLevel: LogLevel.verbose, // or LogLevel.error for less verbose logging
+  environment: Environment.production,
+  logLevel: LogLevel.error,
 });
