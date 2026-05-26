35da26672bd4fdea92ba190e98a33eb45d118949
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-26 01:08:22 -0600

    Update page.tsx

--- src/app/dashboard/documents/page.tsx
+++ src/app/dashboard/documents/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ManageDocumentContainer from "./_components/manage-document-container";
-
 const Page = async () => {
   return (
     <div>
@@ -9,13 +8,17 @@ const Page = async () => {
         <h1 className="text-primary font-semibold text-[32px] leading-[120%]">
           Documents
         </h1>
-        <Button asChild className="min-h-[45px]">
-          <Link href="/dashboard/documents/new">Add Documeent</Link>
-        </Button>
+        <div className="flex gap-3">
+          <Button asChild className="min-h-[45px]" variant="outline">
+            <Link href="/dashboard/documents/import">Import</Link>
+          </Button>
+          <Button asChild className="min-h-[45px]">
+            <Link href="/dashboard/documents/new">Add Document</Link>
+          </Button>
+        </div>
       </div>
       <ManageDocumentContainer />
     </div>
   );
 };
-
 export default Page;
