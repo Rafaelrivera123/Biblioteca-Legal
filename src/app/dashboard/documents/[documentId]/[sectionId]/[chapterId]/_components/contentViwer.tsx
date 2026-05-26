efcd3d39de01b96243e8f934bbb660eec2410224
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-26 17:34:55 -0600

    Update contentViwer.tsx

--- src/app/dashboard/documents/[documentId]/[sectionId]/[chapterId]/_components/contentViwer.tsx
+++ src/app/dashboard/documents/[documentId]/[sectionId]/[chapterId]/_components/contentViwer.tsx
@@ -14,7 +14,7 @@ const ContentViewer = ({ content }: Props) => {
       em: [],
       i: [],
       u: [],
-      ol: [],
+      ol: ["type", "start"],
       ul: [],
       li: [],
       br: [],
@@ -25,7 +25,12 @@ const ContentViewer = ({ content }: Props) => {
   return (
     <div
       className="text-[14px] leading-[200%] space-y-2 
-        [&_ol]:list-decimal [&_ol]:pl-8 [&_ol]:my-2 [&_ol]:space-y-1
+        [&_ol]:pl-8 [&_ol]:my-2 [&_ol]:space-y-1
+        [&_ol[type='a']]:list-[lower-alpha]
+        [&_ol[type='A']]:list-[upper-alpha]
+        [&_ol[type='i']]:list-[lower-roman]
+        [&_ol[type='I']]:list-[upper-roman]
+        [&_ol:not([type])]:list-decimal
         [&_ul]:list-disc [&_ul]:pl-8 [&_ul]:my-2 [&_ul]:space-y-1
         [&_li]:leading-[180%] [&_li]:pl-1"
       dangerouslySetInnerHTML={{
