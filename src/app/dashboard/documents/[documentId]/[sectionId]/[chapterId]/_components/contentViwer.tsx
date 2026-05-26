1dbee0052bdbf1f7545d536ded3be2431e5e5b76
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-26 14:00:38 -0600

    Update contentViwer.tsx

--- src/app/dashboard/documents/[documentId]/[sectionId]/[chapterId]/_components/contentViwer.tsx
+++ src/app/dashboard/documents/[documentId]/[sectionId]/[chapterId]/_components/contentViwer.tsx
@@ -1,21 +1,33 @@
 "use client";
-// import DOMPurify from "dompurify";
 import xss from "xss";
-// const windowDOMPurify = DOMPurify as typeof DOMPurify & {
-//   sanitize: (dirty: string) => string;
-// };
-
-// const { sanitize } = windowDOMPurify;
 
 interface Props {
   content: string;
 }
 
 const ContentViewer = ({ content }: Props) => {
-  const cleanHTML = xss(content);
+  const cleanHTML = xss(content, {
+    whiteList: {
+      p: [],
+      b: [],
+      strong: [],
+      em: [],
+      i: [],
+      u: [],
+      ol: [],
+      ul: [],
+      li: [],
+      br: [],
+      span: ["style"],
+    },
+  });
+
   return (
     <div
-      className="text-[14px] leading-[200%] space-y-5"
+      className="text-[14px] leading-[200%] space-y-2 
+        [&_ol]:list-decimal [&_ol]:pl-8 [&_ol]:my-2 [&_ol]:space-y-1
+        [&_ul]:list-disc [&_ul]:pl-8 [&_ul]:my-2 [&_ul]:space-y-1
+        [&_li]:leading-[180%] [&_li]:pl-1"
       dangerouslySetInnerHTML={{
         __html: cleanHTML,
       }}
