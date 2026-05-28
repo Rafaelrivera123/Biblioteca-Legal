e2a730d58e152614295270766c297d2f647d1613
Rafaelrivera123 <rafariveras10@gmail.com>
2026-05-28 01:09:48 -0600

    Update page.tsx

--- src/app/(website)/collections/[id]/page.tsx
+++ src/app/(website)/collections/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import { prisma } from "@/lib/db";
 import { notFound } from "next/navigation";
 import ArticleContainer from "./_components/article-container";
@@ -6,6 +7,9 @@ import CollectionHeader from "./_components/collection-header";
 export const dynamic = "force-dynamic";
 
 const Page = async ({ params }: { params: { id: string } }) => {
+  const cu = await auth();
+  const isLoggedin = !!cu;
+
   const document = await prisma.document.findFirst({
     where: {
       OR: [
@@ -17,10 +21,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
 
   if (!document) notFound();
 
+  const sections = await prisma.section.findMany({
+    where: { documentId: document.id },
+    include: {
+      chapters: {
+        include: {
+          articles: { orderBy: { articleNumber: "asc" } },
+        },
+      },
+    },
+    orderBy: { createdAt: "asc" },
+  });
+
   return (
     <div>
       <CollectionHeader document={document} hasFullAccess={true} />
-      <ArticleContainer documentId={document.id} isLoggedin={true} />
+      <ArticleContainer
+        documentId={document.id}
+        isLoggedin={isLoggedin}
+        sections={sections}
+      />
     </div>
   );
 };
