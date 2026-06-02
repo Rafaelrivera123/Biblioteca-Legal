a69cc3dfc9da5d2f387a3c0fd13563ad33128e59
Rafaelrivera123 <rafariveras10@gmail.com>
2026-06-01 23:45:43 -0600

    Update route.ts

--- src/app/api/account/highlights/route.ts
+++ src/app/api/account/highlights/route.ts
@@ -1,23 +1,17 @@
 import { auth } from "@/auth";
-import { prisma } from "@/lib/db"; // adjust path to your prisma instance
+import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-
-// Force dynamic rendering
 export const dynamic = "force-dynamic";
-
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
     const cu = await auth();
     const page = parseInt(searchParams.get("page") || "1", 10);
     const limit = parseInt(searchParams.get("limit") || "10", 10);
-
     if (!cu?.user?.id) {
       return NextResponse.json({ error: "Missing userId" }, { status: 400 });
     }
-
     const skip = (page - 1) * limit;
-
     const [data, total] = await Promise.all([
       prisma.userArticleMeta.findMany({
         where: { userId: cu.user.id, selectedColor: { not: null } },
@@ -29,32 +23,23 @@ export async function GET(req: NextRequest) {
             select: {
               name: true,
               short_description: true,
+              slug: true,
+              id: true,
             },
           },
         },
-        orderBy: {
-          createdAt: "desc",
-        },
+        orderBy: { createdAt: "desc" },
       }),
       prisma.userArticleMeta.count({
         where: { userId: cu.user.id, selectedColor: { not: null } },
       }),
     ]);
-
     return NextResponse.json({
       data,
-      pagination: {
-        total,
-        page,
-        limit,
-        totalPages: Math.ceil(total / limit),
-      },
+      pagination: { total, page, limit, totalPages: Math.ceil(total / limit) },
     });
   } catch (error) {
     console.error("Error fetching user article meta:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
