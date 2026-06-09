dd85651451c4c54e00603b02f49783b85bb88820
Rafaelrivera123 <rafariveras10@gmail.com>
2026-06-08 21:11:33 -0600

    Update layout.tsx

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -4,18 +4,19 @@ import Navbar from "@/components/ui/navbar";
 import { prisma } from "@/lib/db";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import NextTopLoader from "nextjs-toploader";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import CookieBanner from "@/components/shared/cookie-banner";
 import { Analytics } from "@vercel/analytics/react";
+import dynamic from "next/dynamic";
+
+const OnboardingTour = dynamic(() => import("@/components/tour/OnboardingTour"), { ssr: false });
 
 const WebsiteLayout = async ({ children }: { children: ReactNode }) => {
   const cu = await auth();
   let user;
   if (cu?.user?.id) {
     user = await prisma.user.findUnique({
-      where: {
-        id: cu.user.id,
-      },
+      where: { id: cu.user.id },
     });
   }
   return (
@@ -27,7 +28,16 @@ const WebsiteLayout = async ({ children }: { children: ReactNode }) => {
       <NextTopLoader showSpinner={false} color="#FFFFFF" />
       <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID!} />
       <Analytics />
+      {cu && user && (
+        <Suspense>
+          <OnboardingTour
+            onboardingCompleted={user.onboardingCompleted}
+            isLoggedin={true}
+          />
+        </Suspense>
+      )}
     </div>
   );
 };
+
 export default WebsiteLayout;
