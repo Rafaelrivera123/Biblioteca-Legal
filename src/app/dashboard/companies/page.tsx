e1fabc43c86f7b60969ad46db9b8baaed53d2d28
Rafaelrivera123 <rafariveras10@gmail.com>
2026-08-01 13:53:01 -0600

    Update page.tsx

--- src/app/dashboard/companies/page.tsx
+++ src/app/dashboard/companies/page.tsx
@@ -5,13 +5,15 @@ import CompanyCardsContainer from "./_components/company-cards-container";
 const Page = () => {
   return (
     <div>
-      <div className="w-full flex justify-between mb-5">
-        <h1 className="text-primary font-semibold text-[32px] leading-[120%]">
+      <div className="w-full flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between mb-5">
+        <h1 className="text-primary font-semibold text-[26px] sm:text-[32px] leading-[120%]">
           Manage Company
         </h1>
 
         <AddCompanyModal
-          trigger={<Button className="h-[45px]">Add Company</Button>}
+          trigger={
+            <Button className="h-[45px] w-full sm:w-auto">Add Company</Button>
+          }
         />
       </div>
 
