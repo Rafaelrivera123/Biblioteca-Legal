b57c3229d7b3ae864fe9ac4eb82b2146f4461177
Rafaelrivera123 <rafariveras10@gmail.com>
2026-08-01 13:59:08 -0600

    Update admin-access-container.tsx

--- src/app/dashboard/settings/_components/admin-access/admin-access-container.tsx
+++ src/app/dashboard/settings/_components/admin-access/admin-access-container.tsx
@@ -26,7 +26,7 @@ const AdminAccessContainer = async () => {
     <div className="w-full shadow-none">
       <Card className="bg-[#FFFFFF] rounded-[15px] shadow-none">
         <CardHeader className="pb-3 pt-5">
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
             <CardTitle className="text-primary text-xl font-semibold">
               Admin Access Share
             </CardTitle>
