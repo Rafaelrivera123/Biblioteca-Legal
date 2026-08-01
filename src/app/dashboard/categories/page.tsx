1e8869ddd2bf621223874c8e8b7837df41f1dc05
Rafaelrivera123 <rafariveras10@gmail.com>
2026-08-01 13:52:32 -0600

    Update page.tsx

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -13,17 +13,19 @@ const Page = async () => {
   const allCategories = await prisma.category.findMany();
   return (
     <div>
-      <div className="w-full flex justify-between mb-5">
-        <h1 className="text-primary font-semibold text-[32px] leading-[120%]">
+      <div className="w-full flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between mb-5">
+        <h1 className="text-primary font-semibold text-[26px] sm:text-[32px] leading-[120%]">
           Manage Category
         </h1>
 
         <AddCategoryDialog
-          trigger={<Button className="h-[45px]">Add Category</Button>}
+          trigger={
+            <Button className="h-[45px] w-full sm:w-auto">Add Category</Button>
+          }
         />
       </div>
 
-      <div className="flex flex-wrap gap-x-[60px] gap-y-[30px] mt-[80px]">
+      <div className="flex flex-wrap gap-x-[60px] gap-y-[30px] mt-10 sm:mt-[80px]">
         {allCategories.map((item) => (
           <CategoryCard key={item.id} data={item} />
         ))}
